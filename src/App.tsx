cc6c7e33d474893e09a0e11ebf2be0ff289f928f
AabhasAgarwal-AA <ag7152@srmist.edu.in>
2025-01-14 22:57:56 +0530

    connected to backend

---
+++ src/App.tsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import "./App.css";
+import { URL } from "./config"
+import axios from "axios";
+
+function App() {
+  const [name, setName] = useState('');
+  const [datetime, setDatetime] = useState('');
+  const [ description, setDescription] = useState('');
+
+  async function handleSubmit(event:any){
+    event.preventDefault()
+    const url = URL+'/api/v1/transaction';
+    console.log(url);
+    const response = await axios.post(`${url}`, {
+      name:name,
+      description: description,
+      datetime:datetime,
+      
+      // body: JSON.stringify(value:{name, description, datetime})
+    },{
+      headers:{'Content-type':'application/json'}
+    });
+
+    const result = response.data.result;
+    console.log(result);
+  }
+
+  return (
+    <>
+      <main>
+        <h1>
+          Rs 400<span>.00</span>
+        </h1>
+        <form onSubmit={handleSubmit}>
+          <div className="basic">
+            <input type="text"
+              value={name}
+              onChange={e => setName(e.target.value)}
+              placeholder={"+200 TV"} />
+            <input type="datetime-local" 
+              value={datetime}
+              onChange={e => setDatetime(e.target.value)} />
+          </div>
+          <div className="description">
+            <input type="text" placeholder={"description"}
+              value={description}
+              onChange={e => setDescription(e.target.value)}/>
+          </div>
+          <button type="submit">Add new transaction</button>
+        </form>
+
+        <div className="transactions">
+          <div className="transaction">
+            <div className="left">
+              <div className="name">New TV</div>
+              <div className="description"> it was time for new tv</div>
+            </div>
+
+            <div className="right">
+              <div className="price red">- 500 rs</div>
+              <div className="datetime">2025-1-112</div>
+            </div>
+          </div>
+
+          <div className="transaction">
+            <div className="left">
+              <div className="name">Earned from gig</div>
+              <div className="description"> it was time for new tv</div>
+            </div>
+
+            <div className="right">
+              <div className="price green">+700 rs</div>
+              <div className="datetime">2025-1-112</div>
+            </div>
+          </div>
+
+          <div className="transaction">
+            <div className="left">
+              <div className="name">Spend</div>
+              <div className="description"> it was time for new tv</div>
+            </div>
+
+            <div className="right">
+              <div className="price red">-900 rs</div>
+              <div className="datetime">2025-1-112</div>
+            </div>
+          </div>
+
+            
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
